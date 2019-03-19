fb86d1036ce1b74d88d5ca9edec2b2750993bef8
HerreraMNicolas <40763507+maatyyazzz@users.noreply.github.com>
2019-03-19 12:06:17 -0300

    clase 1 ejercicio max y min

--- Program.cs
+++ Program.cs
@@ -4,22 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proyecto01
+namespace ejercicio_01
 {
-    class Program
+    class ejercicio_01
     {
         static void Main(string[] args)
         {
-            //Ingreso un nombre y lo muestro en consola:
+            Console.Title= "ejercicio_01";
 
-            string Nombre;
+            int Numeros;
+            int i;
+            int Maximo=0;
+            int Minimo = 0;
+            float Prom = 0;
 
-            Console.Write("Ingrese su nombre: ");
+            for (i = 0; i < 5; i++)
+            {
 
-            Nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese numeros: ");
 
-            Console.WriteLine(Nombre);
+                Numeros = int.Parse(Console.ReadLine());
 
+                if (i == 0)
+                {
+                    Maximo = Numeros;
+                    Minimo = Numeros;
+                }
+               
+                if(Numeros >= Maximo)
+                {
+                    Maximo = Numeros;
+                }
+                else if(Numeros <= Minimo)
+                {
+                    Minimo = Numeros;
+                }
+
+                Prom = Prom + Numeros;
+            }
+
+            Console.WriteLine("maximo:{0} ",Maximo);
+            Console.WriteLine("minimo:{0} ",Minimo);
+            Console.WriteLine("Promedio:{0} ",Prom /5);
             Console.ReadKey();
 
         }
