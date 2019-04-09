c2283ea248bd8bbe8a4dcb2598ea3171a8159f2b
HerreraMNicolas <40763507+maatyyazzz@users.noreply.github.com>
2019-04-09 12:12:52 -0300

    clase_06

---
+++ Paleta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_05.Entidades
+{
+    public class Paleta
+    {
+        private Tempera[] _temperas;
+        private int _cantidadMax;
+
+        private Paleta(int cantidadMaxima)
+        {
+            this._cantidadMax = cantidadMaxima;
+            _temperas = new Tempera[cantidadMaxima];
+        }
+
+        private Paleta() : this(5)
+        {
+
+        }
+
+        public static implicit operator Paleta(int cantidad)
+        {
+            Paleta objRetorno = new Paleta(cantidad);
+
+            return objRetorno;
+        }
+
+        private string Mostrar()
+        {
+            string retorno = "";
+
+            retorno = "la cantidad maxima es: " + this._cantidadMax.ToString();
+
+            foreach(Tempera t in this._temperas)
+            {
+                 retorno += "\n" + Tempera.Mostrar(t);  // Foreach va guardando las temperas en la variable t, _temperas es el array a recorrer.
+            }
+
+            return retorno;
+            
+        }
+
+        public static explicit operator string (Paleta mostrarExplicito)
+        {
+            return mostrarExplicito.Mostrar();
+        }
+
+        
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,53 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 using clase_05.Entidades;
 
-namespace Clase_05.test
+
+namespace Clase_06test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Tinta obj2 = new Tinta(ConsoleColor.Blue);
-            //Tinta obj3 = new Tinta(ETipoTinta.China, ConsoleColor.Red);
 
-            //Console.WriteLine(Tinta.Mostrar(obj1));
-            //Console.WriteLine(Tinta.Mostrar(obj2));
-            //Console.WriteLine(Tinta.Mostrar(obj3));
+            Paleta objPaleta = 8;
+            Tempera objTempera = new Tempera(ConsoleColor.Black, "Super color", 20);
 
-            /* Tinta t1 = new Tinta();
-               Tinta t2 = new Tinta(ConsoleColor.Blue);
-               Tinta t3 = new Tinta(ETipoTinta.China, ConsoleColor.Red);
-               Tinta t4 = new Tinta();
-               Tinta t5 = t1;
-
-
-               Console.WriteLine(Tinta.Mostrar(t1));
-               Console.WriteLine(Tinta.Mostrar(t2));
-               Console.WriteLine(Tinta.Mostrar(t3));
-
-               if(t1 == t4)//Equals se encarga de comparar si los parametros pasados son iguales (de tipo object)
-               {
-                   Console.WriteLine("Son iguales");
-               }
-               else
-               {
-                   Console.WriteLine("No son iguales");
-               }
-
-               */
-
-            Tinta objT = new Tinta();
-            Pluma objP = new Pluma("BIC", 20, objT);
-          
-            
-            objP = objP + objT;
-            string s = (string)objP;
-
-            Console.WriteLine(s);
+            Console.WriteLine((string)objPaleta);
 
             Console.ReadKey();
 
-            
         }
     }
 }
