deb9aa96b059d20ee04422a764e0e81087fa0cf1
alumno <alumno>
2019-05-28 12:01:23 -0300

    Clases genericas

---
+++ laboratorio en clase/Laboratorio-II-master/Herrera.Matias/Clase_16/EntidadesDeposito/Auto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDeposito
+{
+    public class Auto
+    {
+        private string _color;
+        private string _marca;
+
+        public Auto(string color, string marca)
+        {
+            this._color = color;
+            this._marca = marca;
+        }
+
+
+        public string Marca
+        {
+            get { return this._marca; }
+        }
+
+        public string Color
+        {
+            get { return this._color; }
+        }
+
+
+        public static bool operator ==(Auto a1, Auto a2)
+        {
+            bool retorno = false;
+
+            if(a1.Color == a2.Color && a1.Marca == a2.Marca)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Auto a1, Auto a2)
+        {
+            return !(a1 == a2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool retorno = false;
+
+            if(obj is Auto)
+            {
+                if((Auto)obj == this)
+                {
+                    retorno = true;
+                }
+            }
+
+            return retorno;
+        }
+
+        public override string ToString()
+        {
+            return "Color: " + this._color + " - Marca: " + this._marca + "\n";
+        }
+
+
+    }
+}
---
+++ laboratorio en clase/Laboratorio-II-master/Herrera.Matias/Clase_16/EntidadesDeposito/DepositoDeAutos.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesDeposito
+{
+    public class DepositoDeAutos
+    {
+        private int _capacidadMaxima;
+        private List<Auto> _listado;
+
+        public DepositoDeAutos(int capacidad)
+        {
+            _listado = new List<Auto>();
+            this._capacidadMaxima = capacidad;
+        }
+
+        public static bool operator + (DepositoDeAutos d, Auto a)
+        {
+            bool retorno = false;
+
+            if(d._listado.Count < d._capacidadMaxima)
+            {
+                d._listado.Add(a);
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        private int GetIndice(Auto a)
+        {
+            int retorno = -1;
+
+            foreach(Auto au in this._listado)
+            {
+                if(au == a)
+                {
+                    retorno = this._listado.IndexOf(au);
+                }
+            }
+
+            return retorno;
+        }
+
+
+        public static bool operator -(DepositoDeAutos d, Auto a)
+        {
+            bool retorno = false;
+
+            if(d.GetIndice(a) != -1)
+            {
+                d._listado.Remove(a);
+                retorno = true;
+            }
+            
+            return retorno;
+        }
+
+        public bool Agregar(Auto a)
+        {
+            return this + a;
+        }
+
+        public bool Remover(Auto a)
+        {
+            return this - a;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+
+            retorno += "La capacidad es: " + this._capacidadMaxima.ToString() + "\n\n";
+            retorno += "*******************\n";
+            retorno += "Listado de autos\n";
+            retorno += "*******************\n";
+
+            foreach(Auto a in this._listado)
+            {
+                retorno += a.ToString();
+            }
+
+            return retorno;
+        }
+
+    }
+}
