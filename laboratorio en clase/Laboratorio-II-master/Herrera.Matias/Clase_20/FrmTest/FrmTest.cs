3642f838fa4eb55323974b448ad51c535ec7c95c
alumno <alumno>
2019-06-18 11:53:40 -0300

    Clase 20 eventos

---
+++ laboratorio en clase/Laboratorio-II-master/Herrera.Matias/Clase_20/FrmTest/FrmTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmTest
+{
+    public partial class FrmTest : Form
+    {
+        public FrmTest()
+        {
+            InitializeComponent();
+
+            Manejadora m = new Manejadora();
+
+            this.lblEtiqueta.Click += new System.EventHandler(Manejadora.Manejador);
+            this.txtCuadroTexto.Click += new System.EventHandler(Manejadora.Manejador);
+            this.btnBoton.Click += new System.EventHandler(Manejadora.Manejador);
+            this.lblEtiqueta.Click += new System.EventHandler(m.ManejadorZ);
+            this.txtCuadroTexto.Click += new System.EventHandler(m.ManejadorZ);
+            this.btnBoton.Click += new System.EventHandler(m.ManejadorZ);
+        }
+
+        private void mostrarMensaje(object sender, EventArgs e) //Object: quien provoco el evento, EventArgs: 
+        { 
+            MessageBox.Show("Evento click del boton");
+            //this.lblEtiqueta.Click += new System.EventHandler(this.lblEtiqueta_Click); //por cada click en mostrar, agrego una nueva direccion de memoria para la lista de invocacion del label.
+            //this.txtCuadroTexto.Click += new System.EventHandler(this.txtCuadroTexto_Click);
+        }
+
+        private void lblEtiqueta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Evento click del label");
+            //this.lblEtiqueta.Click -= new System.EventHandler(this.lblEtiqueta_Click); //quita los eventos agregados
+
+        }
+
+        private void txtCuadroTexto_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Evento click del text");
+        }
+
+
+
+
+    }
+}
---
+++ laboratorio en clase/Laboratorio-II-master/Herrera.Matias/Clase_20/FrmTest/Manejadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FrmTest;
+
+namespace FrmTest
+{
+    public class Manejadora
+    {
+
+        public static void Manejador(object sender, EventArgs e)
+        {
+            MessageBox.Show("Estoy usando la clase estatica manejadora");
+            
+            if(sender is TextBox)
+            {
+                MessageBox.Show("Fue un text box");
+            }
+            else if(sender is Label)
+            {
+                MessageBox.Show("Fue un label");
+            }
+            else
+            {
+                MessageBox.Show("Fue un button");
+            }
+        }
+
+        public void ManejadorZ(object sender, EventArgs e)
+        {
+            MessageBox.Show("Estoy en el metodo de instancia de la clase manejador");
+
+            if (sender is TextBox)
+            {
+                MessageBox.Show("Fue un text box");
+            }
+            else if (sender is Label)
+            {
+                MessageBox.Show("Fue un label");
+            }
+            else
+            {
+                MessageBox.Show("Fue un button");
+            }
+        }
+
+    }
+}
